10bf86f1ee9ec0eb110069053e48c6c34f04635c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-01-22 14:29:27 +0900

    hmm

--- test/wizard-patch-generates-policy.test.js
+++ test/wizard-patch-generates-policy.test.js
@@ -123,6 +123,8 @@ function npm(method, packages, dir) {
       if (error) {
         return reject(error);
       }
+      
+      console.log(stdout, stderr);
 
       if (stderr.indexOf('ERR!') !== -1) {
         return reject(new Error(stderr.trim()));
